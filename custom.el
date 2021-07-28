63c1f1d7159f844447e3c8a5f4bf8568374c7065
Xianhai Cao <xianhaic@gmail.com>
2021-07-28 09:20:48 -0700

    Add emacs config

--- custom.el
+++ custom.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("01cf34eca93938925143f402c2e6141f03abb341f27d1c2dba3d50af9357ce70" default))
+   '("0685ffa6c9f1324721659a9cd5a8931f4bb64efae9ce43a3dba3801e9412b4d8" "ff3c57a5049010a76de8949ddb629d29e2ced42b06098e046def291989a4104a" "c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" "f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" "ca70827910547eb99368db50ac94556bbd194b7e8311cfbdbdcad8da65e803be" "e3c64e88fec56f86b49dcdc5a831e96782baf14b09397d4057156b17062a8848" "8feca8afd3492985094597385f6a36d1f62298d289827aaa0d8a62fe6889b33c" "01cf34eca93938925143f402c2e6141f03abb341f27d1c2dba3d50af9357ce70" default))
  '(package-selected-packages
-   '(evil-magit magit counsel-projectile projectile hydra evil-collection general doom-themes helpful ivy-rich which-key rainbow-delimiters emacs-leetcode leetcode-emacs leetcode undo-tree doom-modeline counsel use-package ivy evil command-log-mode)))
+   '(inf-elixir evil-surround reformatter elixir-mode elfeed perspective dante oj lsp-ivy lsp-ui lsp-mode company merlin tuareg github-modern-theme ace-jump-mode visual-fill-column visual-fill visual-fill-mode org-superstar org-mode forge evil-magit magit counsel-projectile projectile hydra evil-collection general doom-themes helpful ivy-rich which-key rainbow-delimiters emacs-leetcode leetcode-emacs leetcode undo-tree doom-modeline counsel use-package ivy evil command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -2,6 +2,15 @@
 ;; run the following to update packages list in case a package cant be found
 ;; M-x list-packages
 
+
+;; initializations
+;;;;;;;
+;;;;;;;
+
+;; You will most likely need to adjust this font size for your system!
+(defvar rxpii/default-font-size 110)
+(defvar rxpii/default-variable-font-size 110)
+
 ;; import customize config from a seperate file
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
@@ -24,11 +33,15 @@
 ;;(setq visible-bell t)
 
 ;; fonts
-(set-face-attribute 'default nil :font "Input" :height 110)
+;(set-face-attribute 'default nil :font "Input" :height 110)
+(set-face-attribute 'default nil :font "Fira Code Retina" :height rxpii/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height rxpii/default-font-size)
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height rxpii/default-variable-font-size :weight 'regular)
 
 ;; line numbers (col + row)
 (column-number-mode)
 (global-display-line-numbers-mode t)
+(setq display-line-numbers-type 'relative)
 
 ;; disable line numbers in certain modes
 (dolist (mode '(org-mode-hook
@@ -179,8 +192,96 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+(use-package forge)
+
+;; ergonomics
+(use-package ace-jump-mode)
+
+(use-package evil-surround
+  :config
+  (global-evil-surround-mode 1))
+
+;; org mode!
+(defun rxpii/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(defun rxpii/org-font-setup ()
+  ;; make bullets hyphens
+  (font-lock-add-keywords 'org-mode
+			  '(("^ *\\([-]\\) "
+			     (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; use different font sizes for different header levels
+  (dolist (face '((org-level-1 . 1.2)
+		  (org-level-2 . 1.1)
+		  (org-level-3 . 1.05)
+		  (org-level-4 . 1.0)
+		  (org-level-5 . 1.1)
+		  (org-level-6 . 1.1)
+		  (org-level-7 . 1.1)
+		  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell"  :weight 'bold :height (cdr face)))
+
+  ;; ensure that anything that should be fixed-pitch in org files appears that way
+  (set-face-attribute 'org-block nil    :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil    :inherit 'fixed-pitch)
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil     :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
+  (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . rxpii/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+	org-hide-emphasis-markers t)
+  (rxpii/org-font-setup))
+
+(use-package org-superstar
+  :after org
+  :hook (org-mode . (lambda () (org-superstar-mode 1)))
+  :custom (org-superstar-headline-bullets-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(defun rxpii/org-mode-visual-fill ()
+  (setq visual-fill-column-width 80
+	visual-fill-column-center-text t)
+  (visual-fill-column-mode 1))
+
+(use-package visual-fill-column
+  :defer t
+  :hook (org-mode . rxpii/org-mode-visual-fill))
+
+(use-package oj
+  :ensure t
+  :custom ((oj-default-online-judge 'codeforces)
+	   (oj-home-dir "~/projects/cc/")))
+
+(use-package perspective
+  :config
+  (persp-mode))
+
+(use-package elfeed
+  :config
+  (setq elfeed-feeds
+	'("https://morepablo.com/feeds/all.rss.xml"
+	  "http://planet.lisp.org/rss20.xml"
+	  "https://stevelosh.com/rss.xml"
+	  "https://pvk.ca/atom.xml"
+	  "https://geohot.github.io/blog/feed.xml"))
+  (setq shr-inhibit-images t))
+
 ;; keymappings
 ;;;;;;;;;;;;;;
+
+;; globals
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; spacemacs
 (use-package general
@@ -225,19 +326,125 @@
    "p" '(:ignore t :which-key "projects")
    "pp" '(projectile-switch-project :which-key "switch projects")
 
+   ; speed movement
+   "j" '(:ignore t :which-key "ergonomics")
+   "jj" '(evil-ace-jump-line-mode :which-key "jump to line")
+   "jk" '(evil-ace-jump-char-mode :which-key "jump to char")
+   "jl" '(evil-ace-jump-word-mode :which-key "jump to word")
+
+   ; windows
+   "w" '(:ignore t :which-key "windows")
+   "ws" '(evil-window-split :which-key "window split")
+   "wv" '(evil-window-vsplit :which-key "window vsplit")
+
    ; generic mappings
    ":" '(counsel-M-x :which-key "bring up M-x")
    "." '(find-file :which-key "find file")
-   "SPC" '(projectile-find-file :which-key "find project file"))
-
-;; make esc quit prompts
-;; TODO get rid of these
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-(global-set-key (kbd "C-M-b") 'counsel-switch-buffer)
-
+   "SPC" '(projectile-find-file :which-key "find project file")
+   "TAB" '(persp-switch-by-number :which-key "switch to workspace number"))
 
 ;; prettifiers
 ;;;;;;;;;;;;;;
 
 (use-package doom-themes
-  :config (load-theme 'doom-laserwave t))
+  :config (load-theme 'doom-horizon t))
+
+
+;; language support
+;;;;;;;;;;;;;;;;;;;
+
+;; lsp - the ide of the gods
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  (add-to-list 'exec-path "~/.emacs.d/packages/elixir-ls")
+  :hook ((python-mode . lsp)
+	 (elixir-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui :commands lsp-ui-mode)
+
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+;(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; company completion framework
+(use-package company
+  :hook ((after-init . global-company-mode)
+	 (after-init . company-tng-mode))) ; tab completes rather than cycles
+
+;; ocaml
+(use-package tuareg)
+
+(use-package merlin
+  :after tuareg
+  :hook (tuareg-mode . merlin-mode))
+
+;; haskell
+(use-package dante
+  :after haskell-mode
+  :commands 'dante-mode
+  :init
+  ;(add-hook 'haskell-mode-hook 'flycheck-mode)
+  ;; OR for flymake support:
+  ;(add-hook 'haskell-mode-hook 'flymake-mode)
+  ;(remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
+
+  (add-hook 'haskell-mode-hook 'dante-mode)
+  )
+
+(use-package elixir-mode
+  :init
+  (add-hook 'elixir-mode-hook
+            (lambda ()
+              (push '(">=" . ?\u2265) prettify-symbols-alist)
+              (push '("<=" . ?\u2264) prettify-symbols-alist)
+              (push '("!=" . ?\u2260) prettify-symbols-alist)
+              (push '("==" . ?\u2A75) prettify-symbols-alist)
+              (push '("=~" . ?\u2245) prettify-symbols-alist)
+              (push '("<-" . ?\u2190) prettify-symbols-alist)
+              (push '("->" . ?\u2192) prettify-symbols-alist)
+              (push '("<-" . ?\u2190) prettify-symbols-alist)
+              (push '("|>" . ?\u25B7) prettify-symbols-alist))))
+
+(use-package inf-elixir
+  :bind (("C-c i i" . 'inf-elixir)
+         ("C-c i p" . 'inf-elixir-project)
+         ("C-c i l" . 'inf-elixir-send-line)
+         ("C-c i r" . 'inf-elixir-send-region)
+         ("C-c i b" . 'inf-elixir-send-buffer)))
+
+(use-package reformatter
+  :ensure t
+  :config
+  ; Adds a reformatter configuration called "+elixir-format"
+  ; This uses "mix format -"
+  (reformatter-define +elixir-format
+    :program "mix"
+    :args '("format" "-"))
+  ; defines a function that looks for the .formatter.exs file used by mix format
+  (defun +set-default-directory-to-mix-project-root (original-fun &rest args)
+    (if-let* ((mix-project-root (and buffer-file-name
+                                     (locate-dominating-file buffer-file-name
+                                                             ".formatter.exs"))))
+        (let ((default-directory mix-project-root))
+          (apply original-fun args))
+      (apply original-fun args)))
+  ; adds an advice to the generated function +elxir-format-region that sets the proper root dir
+  ; mix format needs to be run from the root directory otherwise it wont use the formatter configuration
+  (advice-add '+elixir-format-region :around #'+set-default-directory-to-mix-project-root)
+  ; Adds a hook to the major-mode that will add the generated function +elixir-format-on-save-mode
+  ; So, every time we save an elixir file it will try to find a .formatter.exs and then run mix format from
+  ; that file's directory
+  (add-hook 'elixir-mode-hook #'+elixir-format-on-save-mode))
+
+
+;; custom commands
+;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;
+
+;(defun open-daily-tasks () (interactive) (
+(put 'erase-buffer 'disabled nil)
